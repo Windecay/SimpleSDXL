96a05e596e8add3a3d717d466063433859d2572a
windecay <4453636@qq.com>
2025-05-20 02:32:45 +0800

    remove random_aspect_ratio
    Signed-off-by: windecay <4453636@qq.com>

--- webui.py
+++ webui.py
@@ -858,7 +858,7 @@ with shared.gradio_root:
                         image_number = gr.Slider(label='Image Number', minimum=1, maximum=modules.config.default_max_image_number, step=1, value=modules.config.default_image_number)
                         with gr.Accordion(label='Aspect Ratios', open=False, elem_id='aspect_ratios_accordion') as aspect_ratios_accordion:
                             aspect_ratios_selection = gr.Textbox(value='', visible=False) 
-                            random_aspect_ratio_checkbox = gr.Checkbox(label='Random Aspect Ratio', value=False)
+                            # random_aspect_ratio_checkbox = gr.Checkbox(label='Random Aspect Ratio', value=False)
                             aspect_ratios_selections = []
                             for template in flags.aspect_ratios_templates:
                                 aspect_ratios_selections.append(gr.Radio(label='aspect ratios', choices=flags.available_aspect_ratios_list[template], value=flags.default_aspect_ratios[template], visible= template=='SDXL', info='Vertical(9:16), Portrait(4:5), Photo(4:3), Landscape(3:2), Widescreen(16:9), Cinematic(21:9)', elem_classes='aspect_ratios'))
@@ -1725,7 +1725,7 @@ with shared.gradio_root:
                   enhance_input_image, enhance_checkbox, enhance_uov_method, enhance_uov_strength, enhance_uov_processing_order,
                   enhance_uov_prompt_type]
         ctrls += enhance_ctrls
-        ctrls += [random_aspect_ratio_checkbox]
+        # ctrls += [random_aspect_ratio_checkbox]
 
         def parse_meta(raw_prompt_txt, state_params, scene_input_image1, state_is_generating):
             if state_is_generating:
