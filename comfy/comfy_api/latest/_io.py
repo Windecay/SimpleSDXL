c522043f5c6d06179f9c78e00ec14b9e6245675b
windecay <4453636@qq.com>
2025-09-23 19:57:50 +0800

    fix
    Signed-off-by: windecay <4453636@qq.com>

--- comfy/comfy_api/latest/_io.py
+++ comfy/comfy_api/latest/_io.py
@@ -331,7 +331,7 @@ class String(ComfyTypeIO):
             })
 
 @comfytype(io_type="COMBO")
-class Combo(ComfyTypeIO):
+class Combo(ComfyTypeI):
     Type = str
     class Input(WidgetInput):
         """Combo input (dropdown)."""
@@ -360,14 +360,6 @@ class Combo(ComfyTypeIO):
                 "remote": self.remote.as_dict() if self.remote else None,
             })
 
-    class Output(Output):
-        def __init__(self, id: str=None, display_name: str=None, options: list[str]=None, tooltip: str=None, is_output_list=False):
-            super().__init__(id, display_name, tooltip, is_output_list)
-            self.options = options if options is not None else []
-
-        @property
-        def io_type(self):
-            return self.options
 
 @comfytype(io_type="COMBO")
 class MultiCombo(ComfyTypeI):
@@ -736,6 +728,14 @@ class AnyType(ComfyTypeIO):
 
 @comfytype(io_type="MODEL_PATCH")
 class MODEL_PATCH(ComfyTypeIO):
+    Type = Any
+
+@comfytype(io_type="AUDIO_ENCODER")
+class AudioEncoder(ComfyTypeIO):
+    Type = Any
+
+@comfytype(io_type="AUDIO_ENCODER_OUTPUT")
+class AudioEncoderOutput(ComfyTypeIO):
     Type = Any
 
 @comfytype(io_type="COMFY_MULTITYPED_V3")
@@ -1190,18 +1190,13 @@ class _ComfyNodeBaseInternal(_ComfyNodeInternal):
         raise NotImplementedError
 
     @classmethod
-    def validate_inputs(cls, **kwargs) -> bool | str:
-        """Optionally, define this function to validate inputs; equivalent to V1's VALIDATE_INPUTS.
-
-        If the function returns a string, it will be used as the validation error message for the node.
-        """
+    def validate_inputs(cls, **kwargs) -> bool:
+        """Optionally, define this function to validate inputs; equivalent to V1's VALIDATE_INPUTS."""
         raise NotImplementedError
 
     @classmethod
     def fingerprint_inputs(cls, **kwargs) -> Any:
-        """Optionally, define this function to fingerprint inputs; equivalent to V1's IS_CHANGED.
-
-        If this function returns the same value as last run, the node will not be executed."""
+        """Optionally, define this function to fingerprint inputs; equivalent to V1's IS_CHANGED."""
         raise NotImplementedError
 
     @classmethod
@@ -1597,6 +1592,7 @@ class _IO:
     Model = Model
     ClipVision = ClipVision
     ClipVisionOutput = ClipVisionOutput
+    AudioEncoderOutput = AudioEncoderOutput
     StyleModel = StyleModel
     Gligen = Gligen
     UpscaleModel = UpscaleModel
