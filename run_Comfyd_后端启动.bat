08610e7e4837ee6672e1ec66d2d68a0a602e8a22
windecay <4453636@qq.com>
2025-10-17 01:36:32 +0800

    fix path
    Signed-off-by: windecay <4453636@qq.com>

--- run_Comfyd_后端启动.bat
+++ run_Comfyd_后端启动.bat
@@ -1,5 +1,5 @@
 @echo off
 set HF_ENDPOINT=https://hf-mirror.com
-.\python_embeded\python.exe -s SimpleSDXL\comfy\main_comfyd.py  --windows-standalone-build --output-directory ..\..\users\ComfyUI\
+.\python_embeded\python.exe -s SimpleSDXL\comfy\main_comfyd.py  --windows-standalone-build --output-directory ../../users/ComfyUI
 echo All done.
 pause
