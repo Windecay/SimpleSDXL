4b46e3b699bf85d7d054864a192e889f95df9173
windecay <4453636@qq.com>
2025-11-01 01:31:48 +0800

    fix interrogate
    Signed-off-by: windecay <4453636@qq.com>

--- extras/interrogate.py
+++ extras/interrogate.py
@@ -5,7 +5,7 @@ import ldm_patched.modules.model_management as model_management
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
 from modules.model_loader import load_file_from_url
-from modules.config import path_clip_vision
+from modules.config import paths_clip_vision
 from ldm_patched.modules.model_patcher import ModelPatcher
 from extras.BLIP.models.blip import blip_decoder
 from PIL import Image
@@ -28,7 +28,7 @@ class Interrogator:
         if self.blip_model is None:
             filename = load_file_from_url(
                 url='https://huggingface.co/lllyasviel/misc/resolve/main/model_base_caption_capfilt_large.pth',
-                model_dir=path_clip_vision,
+                model_dir=paths_clip_vision[0],
                 file_name='model_base_caption_capfilt_large.pth',
             )
 
