c8497ffcf9e19c4c6e54b663677a7759eaa63457
windecay <4453636@qq.com>
2025-12-16 14:05:20 +0800

    update WanVideoWrapper for SCAIL,upload SCAIL-Pose nodes and workflow
    Signed-off-by: windecay <4453636@qq.com>

--- comfy/custom_nodes/ComfyUI-WanVideoWrapper/nodes_sampler.py
+++ comfy/custom_nodes/ComfyUI-WanVideoWrapper/nodes_sampler.py
@@ -16,6 +16,7 @@ from .utils import(log, print_memory, apply_lora, fourier_filter, optimized_scal
 from .cache_methods.cache_methods import cache_report
 from .nodes_model_loading import load_weights
 from .enhance_a_video.globals import set_enhance_weight, set_num_frames
+from .WanMove.trajectory import replace_feature
 from contextlib import nullcontext
 
 from comfy import model_management as mm
@@ -301,15 +302,6 @@ class WanVideoSampler:
         #I2V
         image_cond = image_embeds.get("image_embeds", None)
         if image_cond is not None:
-            # WanMove
-            wanmove_embeds = image_embeds.get("wanmove_embeds", None)
-            if wanmove_embeds is not None:
-                from .WanMove.trajectory import replace_feature
-                track_pos = wanmove_embeds["track_pos"]
-                if any(not math.isclose(c, 1.0) for c in cfg):
-                    image_cond_neg = torch.cat([image_embeds["mask"], image_cond])
-                image_cond = replace_feature(image_cond.unsqueeze(0).clone(), track_pos.unsqueeze(0), wanmove_embeds.get("strength", 1.0))[0]
-
             if transformer.in_dim == 16:
                 raise ValueError("T2V (text to video) model detected, encoded images only work with I2V (Image to video) models")
             elif transformer.in_dim not in [48, 32]: # fun 2.1 models don't use the mask
@@ -688,50 +680,54 @@ class WanVideoSampler:
         is_looped = False
         context_reference_latent = None
         if context_options is not None:
-            context_schedule = context_options["context_schedule"]
-            context_frames =  (context_options["context_frames"] - 1) // 4 + 1
-            context_stride = context_options["context_stride"] // 4
-            context_overlap = context_options["context_overlap"] // 4
-            context_reference_latent = context_options.get("reference_latent", None)
+            if context_options["context_frames"] <= num_frames:
+                context_schedule = context_options["context_schedule"]
+                context_frames =  (context_options["context_frames"] - 1) // 4 + 1
+                context_stride = context_options["context_stride"] // 4
+                context_overlap = context_options["context_overlap"] // 4
+                context_reference_latent = context_options.get("reference_latent", None)
 
-            # Get total number of prompts
-            num_prompts = len(text_embeds["prompt_embeds"])
-            log.info(f"Number of prompts: {num_prompts}")
-            # Calculate which section this context window belongs to
-            section_size = (latent_video_length / num_prompts) if num_prompts != 0 else 1
-            log.info(f"Section size: {section_size}")
-            is_looped = context_schedule == "uniform_looped"
+                # Get total number of prompts
+                num_prompts = len(text_embeds["prompt_embeds"])
+                log.info(f"Number of prompts: {num_prompts}")
+                # Calculate which section this context window belongs to
+                section_size = (latent_video_length / num_prompts) if num_prompts != 0 else 1
+                log.info(f"Section size: {section_size}")
+                is_looped = context_schedule == "uniform_looped"
 
-            if mocha_embeds is not None:
-                seq_len = (context_frames * 2 + 1 + mocha_num_refs) * (noise.shape[2] * noise.shape[3] // 4)
+                if mocha_embeds is not None:
+                    seq_len = (context_frames * 2 + 1 + mocha_num_refs) * (noise.shape[2] * noise.shape[3] // 4)
+                else:
+                    seq_len = math.ceil((noise.shape[2] * noise.shape[3]) / 4 * context_frames)
+                log.info(f"context window seq len: {seq_len}")
+
+                if context_options["freenoise"]:
+                    log.info("Applying FreeNoise")
+                    # code from AnimateDiff-Evolved by Kosinkadink (https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved)
+                    delta = context_frames - context_overlap
+                    for start_idx in range(0, latent_video_length-context_frames, delta):
+                        place_idx = start_idx + context_frames
+                        if place_idx >= latent_video_length:
+                            break
+                        end_idx = place_idx - 1
+
+                        if end_idx + delta >= latent_video_length:
+                            final_delta = latent_video_length - place_idx
+                            list_idx = torch.tensor(list(range(start_idx,start_idx+final_delta)), device=torch.device("cpu"), dtype=torch.long)
+                            list_idx = list_idx[torch.randperm(final_delta, generator=seed_g)]
+                            noise[:, place_idx:place_idx + final_delta, :, :] = noise[:, list_idx, :, :]
+                            break
+                        list_idx = torch.tensor(list(range(start_idx,start_idx+delta)), device=torch.device("cpu"), dtype=torch.long)
+                        list_idx = list_idx[torch.randperm(delta, generator=seed_g)]
+                        noise[:, place_idx:place_idx + delta, :, :] = noise[:, list_idx, :, :]
+
+                log.info(f"Context schedule enabled: {context_frames} frames, {context_stride} stride, {context_overlap} overlap")
+                from .context_windows.context import get_context_scheduler, create_window_mask, WindowTracker
+                self.window_tracker = WindowTracker(verbose=context_options["verbose"])
+                context = get_context_scheduler(context_schedule)
             else:
-                seq_len = math.ceil((noise.shape[2] * noise.shape[3]) / 4 * context_frames)
-            log.info(f"context window seq len: {seq_len}")
-
-            if context_options["freenoise"]:
-                log.info("Applying FreeNoise")
-                # code from AnimateDiff-Evolved by Kosinkadink (https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved)
-                delta = context_frames - context_overlap
-                for start_idx in range(0, latent_video_length-context_frames, delta):
-                    place_idx = start_idx + context_frames
-                    if place_idx >= latent_video_length:
-                        break
-                    end_idx = place_idx - 1
-
-                    if end_idx + delta >= latent_video_length:
-                        final_delta = latent_video_length - place_idx
-                        list_idx = torch.tensor(list(range(start_idx,start_idx+final_delta)), device=torch.device("cpu"), dtype=torch.long)
-                        list_idx = list_idx[torch.randperm(final_delta, generator=seed_g)]
-                        noise[:, place_idx:place_idx + final_delta, :, :] = noise[:, list_idx, :, :]
-                        break
-                    list_idx = torch.tensor(list(range(start_idx,start_idx+delta)), device=torch.device("cpu"), dtype=torch.long)
-                    list_idx = list_idx[torch.randperm(delta, generator=seed_g)]
-                    noise[:, place_idx:place_idx + delta, :, :] = noise[:, list_idx, :, :]
-
-            log.info(f"Context schedule enabled: {context_frames} frames, {context_stride} stride, {context_overlap} overlap")
-            from .context_windows.context import get_context_scheduler, create_window_mask, WindowTracker
-            self.window_tracker = WindowTracker(verbose=context_options["verbose"])
-            context = get_context_scheduler(context_schedule)
+                log.info("Context frames is larger than total num_frames, disabling context windows")
+                context_options = None
 
         #MTV Crafter
         mtv_input = image_embeds.get("mtv_crafter_motion", None)
@@ -1227,6 +1223,28 @@ class WanVideoSampler:
                 latents_to_not_step = prev_latents.shape[1]
                 one_to_all_data["num_latent_frames_to_replace"] = latents_to_not_step
 
+        # SCAIL
+        scail_embeds = image_embeds.get("scail_embeds", None)
+        scail_data = None
+        if scail_embeds is not None:
+            log.info("Using SCAIL embeddings:")
+            for k, v in scail_embeds.items():
+                log.info(f"  {k}: {v.shape if isinstance(v, torch.Tensor) else v}")
+            scail_data = scail_embeds.copy()
+            scail_data = dict_to_device(scail_data, device, dtype)
+
+
+        # WanMove
+        wanmove_embeds = None
+        if image_cond is not None:
+            wanmove_embeds = image_embeds.get("wanmove_embeds", None)
+            if wanmove_embeds is not None:
+                track_pos = wanmove_embeds["track_pos"]
+                if any(not math.isclose(c, 1.0) for c in cfg):
+                    image_cond_neg = torch.cat([image_embeds["mask"], image_cond])
+                if context_options is None:
+                    image_cond = replace_feature(image_cond.unsqueeze(0).clone(), track_pos.unsqueeze(0), wanmove_embeds.get("strength", 1.0))[0]
+
         #region model pred
         def predict_with_cfg(z, cfg_scale, positive_embeds, negative_embeds, timestep, idx, image_cond=None, clip_fea=None,
                              control_latents=None, vace_data=None, unianim_data=None, audio_proj=None, control_camera_latents=None,
@@ -1465,6 +1483,19 @@ class WanVideoSampler:
                 if background_latents is not None or foreground_latents is not None:
                     z = torch.cat([z, foreground_latents.to(z), background_latents.to(z)], dim=0)
 
+                scail_data_in = None
+                if scail_data is not None:
+                    ref_concat_mask = torch.zeros_like(z[:4])
+                    z = torch.cat([z, ref_concat_mask])
+                    if context_window is not None:
+                        scail_data_in = scail_data.copy()
+                        scail_data_in["pose_latent"] = scail_data["pose_latent"][:, context_window]
+                    else:
+                        scail_data_in = scail_data
+
+                if wanmove_embeds is not None and context_window is not None:
+                    image_cond_input = replace_feature(image_cond_input.unsqueeze(0), track_pos[:, context_window].unsqueeze(0), wanmove_embeds.get("strength", 1.0))[0]
+
                 base_params = {
                     'x': [z], # latent
                     'y': [image_cond_input] if image_cond_input is not None else None, # image cond
@@ -1524,6 +1555,7 @@ class WanVideoSampler:
                     "sdancer_input": sdancer_input, # SteadyDancer input
                     "one_to_all_input": one_to_all_data, # One-to-All input
                     "one_to_all_controlnet_strength": one_to_all_data["controlnet_strength"] if one_to_all_data is not None else 0.0,
+                    "scail_input": scail_data_in, # SCAIL input
                 }
 
                 batch_size = 1
