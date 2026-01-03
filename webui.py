a7c2f5d444b4b20a388c940e3e2335b0345d4077
windecay <4453636@qq.com>
2026-01-03 23:04:45 +0800

    fix
    Signed-off-by: windecay <4453636@qq.com>

--- webui.py
+++ webui.py
@@ -2342,7 +2342,15 @@ with shared.gradio_root:
             unique_id = f"comp_{int(time.time() * 1000)}"
             html = f"""
             <div id="{unique_id}" class="comparison-wrapper"
-                onclick="this.classList.toggle('fullscreen-mode');"
+                onclick="
+                    this.classList.toggle('fullscreen-mode');
+                    var outer = this.querySelector('.outer-img');
+                    var inner = this.querySelector('.inner-img');
+                    if(outer && inner) {{
+                        inner.style.width = outer.offsetWidth + 'px';
+                        inner.style.height = outer.offsetHeight + 'px';
+                    }}
+                "
                 onmousemove="
                     var rect = this.querySelector('.comparison-content').getBoundingClientRect();
                     var x = event.clientX - rect.left;
@@ -2380,8 +2388,13 @@ with shared.gradio_root:
                                  }}
                              }}
                              sync();
-                             setInterval(sync, 500); // Periodic check for safety
-                             window.addEventListener('resize', sync);
+                             if(window.ResizeObserver) {{
+                                 const ro = new ResizeObserver(sync);
+                                 ro.observe(outer);
+                             }} else {{
+                                 setInterval(sync, 100); 
+                                 window.addEventListener('resize', sync);
+                             }}
                         }}
                      "
                 />
@@ -2400,11 +2413,11 @@ with shared.gradio_root:
                  return gr.update(visible=True, size='sm')
             return gr.update(visible=False, size='sm')
 
-        compare_btn.click(toggle_comparison, inputs=[comparison_state, cached_input_image, progress_gallery], outputs=[comparison_state, comparison_box, progress_gallery])
+        compare_btn.click(toggle_comparison, inputs=[comparison_state, cached_input_image, progress_gallery], outputs=[comparison_state, comparison_box, progress_gallery], show_progress=False)
         protections = [random_button, super_prompter, background_theme, image_tools_checkbox] + nav_bars
         generate_button.click(lambda v, a: (v, a, gr.update(value=None, visible=False), gr.update(value=None, visible=False), gr.update(visible=True if v else False), gr.update(visible=True if a else False)), inputs=[scene_video, scene_audio], outputs=[scene_video_backup, scene_audio_backup, scene_video, scene_audio, scene_video_placeholder, scene_audio_placeholder], queue=False, show_progress=False) \
             .then(cache_input_image_func, inputs=[current_tab, uov_input_image, inpaint_input_image, layer_input_image, enhance_input_image, scene_input_image1, scene_canvas_image], outputs=[cached_input_image]) \
-            .then(lambda: gr.update(visible=False, size='sm'), outputs=[compare_btn]) \
+            .then(lambda: (False, gr.update(visible=False), gr.update(visible=True), gr.update(visible=False, size='sm')), outputs=[comparison_state, comparison_box, progress_gallery, compare_btn]) \
             .then(topbar.process_before_generation, inputs=[state_topbar, seed_random, image_seed, params_backend] + scene_params[:15] + [scene_video_backup, scene_audio_backup], outputs=[stop_button, skip_button, generate_button, gallery, state_is_generating, index_radio, image_toolbox, prompt_info_box, image_seed] + protections + [preset_store, identity_dialog], show_progress=False) \
             .then(topbar.wait_for_minicpm_completion, outputs=[], show_progress=False) \
             .then(topbar.avoid_empty_prompt_for_scene, inputs=[prompt, state_topbar, scene_input_image1, scene_theme, scene_additional_prompt, scene_additional_prompt_2], outputs=prompt, show_progress=True) \
@@ -2427,6 +2440,7 @@ with shared.gradio_root:
         ctrls_preview = [debug_true_state if c == debugging_cn_preprocessor else c for c in ctrls]
 
         preview_preprocessing.click(lambda v, a: (v, a, gr.update(value=None, visible=False), gr.update(value=None, visible=False), gr.update(visible=True if v else False), gr.update(visible=True if a else False)), inputs=[scene_video, scene_audio], outputs=[scene_video_backup, scene_audio_backup, scene_video, scene_audio, scene_video_placeholder, scene_audio_placeholder], queue=False, show_progress=False) \
+            .then(lambda: (False, gr.update(visible=False), gr.update(visible=True), gr.update(visible=False, size='sm')), outputs=[comparison_state, comparison_box, progress_gallery, compare_btn]) \
             .then(topbar.process_before_generation, inputs=[state_topbar, seed_random, image_seed, params_backend] + scene_params[:14] + [scene_params[16], scene_video_backup, scene_audio_backup], outputs=[stop_button, skip_button, generate_button, gallery, state_is_generating, index_radio, image_toolbox, prompt_info_box, image_seed] + protections + [preset_store, identity_dialog], show_progress=False) \
             .then(fn=get_task, inputs=ctrls_preview, outputs=currentTask) \
             .then(fn=generate_clicked, inputs=[currentTask, state_topbar], outputs=[progress_html, progress_window, progress_gallery, progress_video, gallery]) \
