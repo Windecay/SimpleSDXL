f29a91b41a0b55cfeae88126a91dbf11a34fd4ec
windecay <4453636@qq.com>
2026-01-31 03:18:39 +0800

    fix

--- launch.py
+++ launch.py
@@ -96,7 +96,7 @@ def check_base_environment():
     print(f'{now_string()} ✦ | 兴趣使然的版本 | ✦ by冰華 ✦')
 
     base_pkg = "simpleai_base"
-    ver_required = "0.3.32"
+    ver_required = "0.3.33"
     REINSTALL_BASE = False #if '_dev' not in version.get_branch() else True
     base_branch = "dev"
     # if '--dev' in (sys.argv):
